461c800d5201bbcd7325df7cf8c9ce6ece8a78ac
dsslleagion <dionisiosamuel61@gmail.com>
2025-02-25 11:25:34 -0300

    25_02_2025

---
+++ iniciando_na_programacao_python-logica_de_programacao_basica/Docstrings_como_comentarios/aula.py
@@ -0,0 +1,21 @@
+"""
+isso não é um comentario 
+
+Docstring
+
+E escrever o que eu 
+quiser 
+
+"""
+
+'''
+
+outro metodo de DocString
+
+'''
+
+# permite escrever um comentario
+# print é imprimir
+print(1+1) # Na Frente
+# Abaixo
+print(123)
---
+++ iniciando_na_programacao_python-logica_de_programacao_basica/Interpretador_Comentarios_de_codigo/aula1.py
@@ -0,0 +1,6 @@
+# permite escrever um comentario
+# print é imprimir
+print(1+1) # Na Frente
+# Abaixo
+print(123)
+
