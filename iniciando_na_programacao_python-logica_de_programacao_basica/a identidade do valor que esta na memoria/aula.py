1750630063800dfb45602bc2fcd54b5269cdef01
dsslleagion <dionisiosamuel61@gmail.com>
2025-07-31 16:40:26 -0300

    31_07_2025

---
+++ iniciando_na_programacao_python-logica_de_programacao_basica/a identidade do valor que esta na memoria/aula.py
@@ -0,0 +1,20 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = não valor 
+is e is not = é ou não é (tipo, valor, identidade)
+id = identidade 
+"""
+
+v1 = 'a'
+v2 = 'a'
+print(id(v1))
+print(id(v2))
+
+
+
+# condicao = False
+
+# if condicao:
+#     print('Faça')
+# else:
+#     print('Não faça algo')
